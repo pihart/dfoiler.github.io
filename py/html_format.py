8a0a75ebaea69c5e8e59dcb18dbd7b22af229488
dFoiler <xxtell.noonexx@gmail.com>
2021-02-23 01:07:46 -0600

    minor py reorganization

---
+++ py/html_format.py
@@ -0,0 +1,94 @@
+import os			# path, chdir, listdir, mkdir
+
+# Get absolute path
+PATH = os.path.abspath(__file__)
+PATH = PATH[:-PATH[::-1].index('/')] + '../'
+os.chdir(PATH)
+
+# standalone is for making tikz images
+standalone = open('standalone.sty').read()
+# Generating our header
+header = '''<!DOCTYPE html>
+<html>
+	<head>
+		<title>Today I Learned</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+		<link rel="stylesheet" href="https://dfoiler.github.io/css/main.css">
+		<link rel="stylesheet" href="https://dfoiler.github.io/css/sidebar.css">
+		<link rel=icon href="https://dfoiler.github.io/p.ico">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://dfoiler.github.io/js/mathjax-config.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_SVG-full"></script>
+	</head>\n'''
+
+# Dirty hack to get the month
+months = ['January','February','March','April','May','April','June'
+	'July','August','September','October','November','December']
+
+# Generating the archive
+archive = ''
+for year in next(os.walk('TeX'))[1]:
+	year_months = sorted([int(m[:-len('.tex')]) for m in os.listdir('TeX/'+year)])
+	archive += '<button class="yearmenu">'+year+'</button>\n'
+	archive += '<ul class="monthmenu">\n'
+	for month in year_months:
+		directory = year + '/'+str(month)
+		title = months[month-1] + ' ' + year
+		archive += '<li><span><a href="https://dfoiler.github.io/TIL/' \
+			+directory+'/">'+title+'</a></span></li>\n'
+	archive += '</ul>\n'
+
+# Generating the top HTML
+start_html = header
+start_html += '<body>\n'
+start_html += '<h1 class="title"><a href="https://dfoiler.github.io/TIL/"' \
+	' class="title">Today I Learned</a></h1>\n'
+start_html += '<div class="container">\n'
+# Add in the archive sidebar
+start_html += '<div class="sidebar">\n'
+start_html += '<p style="text-align: center; font-weight: bold; '\
+	'margin-top: 5px;">Archive</p>\n'
+start_html += archive
+start_html += '</div>\n'
+start_html += '<div class="content">\n'
+
+# Generating the bottom HTML
+end_html = '</div>\n'
+end_html += '</div>\n'
+end_html += '<script src="https://dfoiler.github.io/js/sidebar.js"></script>\n'
+end_html += '</body>\n'
+end_html += '</html>\n'
+
+# Reducing my headaches
+def prettify(html):
+	# Standardize input
+	html = html.replace('\n','').replace('\t','')
+	# Gather tags and content
+	content = []
+	for c in html:
+		# New tag
+		if c == '<':
+			content += ['<']
+		# Close tag
+		elif c == '>':
+			content[-1] += '>'
+			content += ['']
+		# We want the tag type after "<": no "<  p>"
+		elif not(content[-1] == '<' and c==' '):
+			content[-1] += c
+	content = [c.strip() for c in content if c]
+	# Indents
+	level = 0
+	# Not exhaustive, but good enough for now
+	no_close = ['</','<img','<!','<link','<meta']
+	html = ''
+	for c in content:
+		# Hit a close: now indent less
+		if '</' in c:
+			level -= 1		
+		html += level*'\t' + c + '\n'
+		# Hit an open: next get indented more
+		if '<' in c and not any(tag in c for tag in no_close):
+			level += 1
+	return html
--- py/textohtml.py
+++ py/textohtml.py
@@ -1,99 +1,17 @@
 import sys			# argv
 import os			# path, chdir, listdir, mkdir
 
-# Get absolute path
+# Relocate for local imports
 PATH = os.path.abspath(__file__)
 PATH = PATH[:-PATH[::-1].index('/')]
 os.chdir(PATH)
 
-# standalone is for making tikz images
-standalone = open('standalone.sty').read()
-# Generating our header
-header = '''<!DOCTYPE html>
-<html>
-	<head>
-		<title>Today I Learned</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-		<link rel="stylesheet" href="https://dfoiler.github.io/css/main.css">
-		<link rel="stylesheet" href="https://dfoiler.github.io/css/sidebar.css">
-		<link rel=icon href="https://dfoiler.github.io/p.ico">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="https://dfoiler.github.io/js/mathjax-config.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_SVG-full"></script>
-	</head>\n'''
+from html_format import *
 
-# Dirty hack to get the month
-months = ['January','February','March','April','May','April','June'
-	'July','August','September','October','November','December']
-
-# Generating the archive
-archive = ''
-for year in next(os.walk('TeX'))[1]:
-	year_months = sorted([int(m[:-len('.tex')]) for m in os.listdir('TeX/'+year)])
-	archive += '<button class="yearmenu">'+year+'</button>\n'
-	archive += '<ul class="monthmenu">\n'
-	for month in year_months:
-		directory = year + '/'+str(month)
-		title = months[month-1] + ' ' + year
-		archive += '<li><span><a href="https://dfoiler.github.io/TIL/' \
-			+directory+'/">'+title+'</a></span></li>\n'
-	archive += '</ul>\n'
-
-def start_html(level):
-	html = header
-	html += '<body>\n'
-	html += '<h1 class="title"><a href="https://dfoiler.github.io/TIL/"' \
-		' class="title">Today I Learned</a></h1>\n'
-	html += '<div class="container">\n'
-	# Add in the archive sidebar
-	html += '<div class="sidebar">\n'
-	html += '<p style="text-align: center; font-weight: bold; '\
-		'margin-top: 5px;">Archive</p>\n'
-	html += archive
-	html += '</div>\n'
-	html += '<div class="content">\n'
-	return html
-
-def end_html(level):
-	html = '</div>\n'
-	html += '</div>\n'
-	html += '<script src="https://dfoiler.github.io/js/sidebar.js"></script>\n'
-	html += '</body>\n'
-	html += '</html>\n'
-	return html
-
-def prettify(html):
-	# Standardize input
-	html = html.replace('\n','').replace('\t','')
-	# Gather tags and content
-	content = []
-	for c in html:
-		# New tag
-		if c == '<':
-			content += ['<']
-		# Close tag
-		elif c == '>':
-			content[-1] += '>'
-			content += ['']
-		# We want the tag type after "<": no "<  p>"
-		elif not(content[-1] == '<' and c==' '):
-			content[-1] += c
-	content = [c.strip() for c in content if c]
-	# Indents
-	level = 0
-	# Not exhaustive, but good enough for now
-	no_close = ['</','<img','<!','<link','<meta']
-	html = ''
-	for c in content:
-		# Hit a close: now indent less
-		if '</' in c:
-			level -= 1		
-		html += level*'\t' + c + '\n'
-		# Hit an open: next get indented more
-		if '<' in c and not any(tag in c for tag in no_close):
-			level += 1
-	return html
+# Relocate for the rest of the program
+PATH = os.path.abspath(__file__)
+PATH = PATH[:-PATH[::-1].index('/')] + '../'
+os.chdir(PATH)
 
 no_images = '-noimg' in sys.argv
 def process_img(tex):
@@ -239,7 +157,7 @@ def to_html(tex):
 	# Now we work line-by-line
 	tex = tex.split('\n')
 	while tex[-1] == '': tex = tex[:-1]
-	day_html = start_html(4)
+	day_html = start_html
 	month_html = '<div class="entry">\n'
 	# We work line-by-line
 	first_item = False
@@ -264,7 +182,7 @@ def to_html(tex):
 			day_html += to_add
 	if last_tags[-1]:
 		day_html += '</'+last_tags[-1]+'>\n'
-	day_html += '</div>\n' + end_html(4)
+	day_html += '</div>\n' + end_html
 	month_html += '<p>'+blurb+'\n'
 	month_html += '<a href="'+day+'/" class="link">(continue reading...)</a></p>\n'
 	month_html += '</div>\n'
@@ -275,7 +193,7 @@ if 'TIL' not in os.listdir():
 	os.mkdir('TIL')
 
 # Start the total file
-total_html = start_html(1)
+total_html = start_html
 # Add in the intro
 total_html += '<h2>Welcome</h2>\n'
 intro = process_tex(open('TeX/intro.tex').read())
@@ -291,7 +209,7 @@ total_html += archive
 total_html += '<div style="height: 6pt;"></div>\n'
 total_html += '</div>\n'
 total_html += '<div style="height: 7pt;"></div>\n'
-total_html += end_html(1)
+total_html += end_html
 # Make the total file
 f = open('TIL/index.html','w')
 f.write(prettify(total_html))
@@ -300,7 +218,7 @@ f.close()
 # Iterate through the years subdirectories
 for year in next(os.walk('TeX'))[1]:
 	# Start the year file
-	year_html = start_html(2)
+	year_html = start_html
 	year_html += '<p class="mobilenav"><a href="../" class="link">' \
 		'(back up to main page) </a></p>\n'
 	year_html += '<h2><a href="./">'+year+'</a></h2>\n'
@@ -315,7 +233,7 @@ for year in next(os.walk('TeX'))[1]:
 		if month not in os.listdir('TIL/'+year):
 			os.mkdir('TIL/'+year+'/'+month)
 		# Start the month file
-		month_html = start_html(3)
+		month_html = start_html
 		month_html += '<p class="mobilenav"><a href="../../" class="link">' \
 			'(back up to main page)</a></p>\n'
 		month_html += '<h2>' + months[int(month)-1] + ' '+year+'</h2>\n'
@@ -343,14 +261,14 @@ for year in next(os.walk('TeX'))[1]:
 			f.close()
 			# Add onto the month
 			month_html += month_html_day
-		month_html += end_html(3)
+		month_html += end_html
 		# Make the month file
 		f = open('TIL/'+year+'/'+month+'/'+'index.html','w')
 		f.write(prettify(month_html))
 		f.close()
 	# Some padding
 	year_html += '<div style="height: 6pt;"></div>\n'
-	year_html += end_html(2)
+	year_html += end_html
 	# Make the year file
 	f = open('TIL/'+year+'/index.html','w')
 	f.write(prettify(year_html))
