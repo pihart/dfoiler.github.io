51a91df18c2cfe42948d8b71d28bb7f5d0b9edf9
dFoiler <xxtell.noonexx@gmail.com>
2021-02-23 00:39:32 -0600

    minor code quality improvements

--- textohtml.py
+++ textohtml.py
@@ -31,62 +31,68 @@ months = ['January','February','March','April','May','April','June'
 archive = ''
 for year in next(os.walk('TeX'))[1]:
 	year_months = sorted([int(m[:-len('.tex')]) for m in os.listdir('TeX/'+year)])
-	archive += '{0}<button class="yearmenu">'+year+'</button>\n'
-	archive += '{0}<ul class="monthmenu">\n'
+	archive += '<button class="yearmenu">'+year+'</button>\n'
+	archive += '<ul class="monthmenu">\n'
 	for month in year_months:
 		directory = year + '/'+str(month)
 		title = months[month-1] + ' ' + year
-		archive += '{0}\t<li><span><a href="https://dfoiler.github.io/TIL/' \
+		archive += '<li><span><a href="https://dfoiler.github.io/TIL/' \
 			+directory+'/">'+title+'</a></span></li>\n'
-	archive += '{0}</ul>\n'
+	archive += '</ul>\n'
 
-indent = lambda n : (0)*'\t'
 def start_html(level):
 	html = header
-	html += '\t<body>\n'
-	html += '\t\t<h1 class="title"><a href="https://dfoiler.github.io/TIL/"' \
+	html += '<body>\n'
+	html += '<h1 class="title"><a href="https://dfoiler.github.io/TIL/"' \
 		' class="title">Today I Learned</a></h1>\n'
-	html += '\t\t<div class="container">\n'
+	html += '<div class="container">\n'
 	# Add in the archive sidebar
-	html += '\t\t\t<div class="sidebar">\n'
-	html += '\t\t\t\t<p style="text-align: center; font-weight: bold; '\
+	html += '<div class="sidebar">\n'
+	html += '<p style="text-align: center; font-weight: bold; '\
 		'margin-top: 5px;">Archive</p>\n'
-	html += archive.format(indent(0))
-	html += '\t\t\t</div>\n'
-	html += '\t\t\t<div class="content">\n'
-	return html
-
-def prettify(html):
-	html = html.replace('\n','').replace('\t','')
-	content = []
-	for c in html:
-		if c == '<':
-			content += ['<']
-		elif c == '>':
-			content[-1] += '>'
-			content += ['']
-		else:
-			# We want to put the tag type right after <
-			if not(content[-1] == '<' and c==' '):
-				content[-1] += c
-	content = [c.strip() for c in content if c]
-	level = 0
-	no_close = ['</','<img','<!','<link','<meta']
-	html = ''
-	for c in content:
-		if '</' in c:
-			level -= 1		
-		html += level*'\t' + c + '\n'
-		if '<' in c and not any(n in c for n in no_close):
-			level += 1
+	html += archive
+	html += '</div>\n'
+	html += '<div class="content">\n'
 	return html
 
 def end_html(level):
-	html = '\t\t\t</div>\n'
-	html += '\t\t</div>\n'
-	html += '\t\t<script src="https://dfoiler.github.io/js/sidebar.js"></script>\n'
-	html += '\t</body>\n'
+	html = '</div>\n'
+	html += '</div>\n'
+	html += '<script src="https://dfoiler.github.io/js/sidebar.js"></script>\n'
+	html += '</body>\n'
 	html += '</html>\n'
+	return html
+
+def prettify(html):
+	# Standardize input
+	html = html.replace('\n','').replace('\t','')
+	# Gather tags and content
+	content = []
+	for c in html:
+		# New tag
+		if c == '<':
+			content += ['<']
+		# Close tag
+		elif c == '>':
+			content[-1] += '>'
+			content += ['']
+		# We want the tag type after "<": no "<  p>"
+		elif not(content[-1] == '<' and c==' '):
+			content[-1] += c
+	content = [c.strip() for c in content if c]
+	# Indents
+	level = 0
+	# Not exhaustive, but good enough for now
+	no_close = ['</','<img','<!','<link','<meta']
+	html = ''
+	for c in content:
+		# Hit a close: now indent less
+		if '</' in c:
+			level -= 1		
+		html += level*'\t' + c + '\n'
+		# Hit an open: next get indented more
+		if '<' in c and not any(tag in c for tag in no_close):
+			level += 1
 	return html
 
 no_images = '-noimg' in sys.argv
@@ -171,9 +177,7 @@ def get_blurb(tex):
 	blurb = blurb[:end].strip()
 	return blurb
 
-# TODO: make a last_tag stack for more memory
-# TODO: make each list item start a paragraph
-def process_line(line, i, last_tags, first_item):
+def process_line(line, last_tags, first_item):
 	to_add = ''
 	close = last_tags[-1] != '' and not first_item
 	new_tags = []
@@ -181,54 +185,50 @@ def process_line(line, i, last_tags, first_item):
 	environs = ['itemize','enumerate']
 	# Consecutive lines implies a new paragraph
 	if line == '':
-		to_add += indent(i)+'<p>'
+		to_add += '<p>'
 		new_tags = ['p']
 	# Various list commands
 	elif '\\begin{itemize}' in line:
-		to_add += indent(i)+'<ul>\n'+indent(i+1)+'<li>'
+		to_add += '<ul>\n'+'<li>'
 		new_tags = ['ul','li']
 	elif '\\end{itemize}' in line:
-		to_add += indent(i-1)+'</ul>\n'
+		to_add += '</ul>\n'
 	elif '\\begin{enumerate}' in line:
-		to_add += indent(i)+'<ol>\n'+indent(i+1)+'<li>'
+		to_add += '<ol>\n'+'<li>'
 		new_tags = ['ol','li']
 	elif '\\end{enumerate}' in line:
-		to_add += indent(i-1)+'</ol>\n'
+		to_add += '</ol>\n'
 		first_item =  False
 	elif '\\item' in line:
 		line = line.replace('\\item','')
 		if not first_item:
-			to_add += indent(i)+'<li>'
+			to_add += '<li>'
 			new_tags = ['li']
 		to_add += line
 		first_item = False
 	# Image marker
 	elif line[0] == '\00':
 		filename = line[1:]
-		to_add += indent(i)+'<img src="'+filename+'">\n'
+		to_add += '<img src="'+filename+'">\n'
 	# Catch-all
 	else:
 		close = False
 		if last_tags[-1] == '':
-			to_add += indent(i)+'<p>'
+			to_add += '<p>'
 			new_tags = ['p']
 		to_add += line
 	# Update beginning indents later
 	if any('\\begin{'+env in line for env in environs):
-		i += 1
 		first_item = True
 		# Close if we're coming from a paragraph
 		close = last_tags[-1] == 'p'
-		if not close:
-			to_add = '\n'+to_add
 	# Add in the closing
 	if close:
 		to_add = '</'+last_tags.pop()+'>\n'+to_add
 	if any('\\end{'+env in line for env in environs):
-		i -= 1
 		last_tags.pop()
 	last_tags += new_tags
-	return to_add, i, last_tags, first_item
+	return to_add, last_tags, first_item
 
 def to_html(tex):
 	# This is not general-purpose and will not work with your TeX
@@ -240,7 +240,7 @@ def to_html(tex):
 	tex = tex.split('\n')
 	while tex[-1] == '': tex = tex[:-1]
 	day_html = start_html(4)
-	month_html = indent(0)+'<div class="entry">\n'
+	month_html = '<div class="entry">\n'
 	# We work line-by-line
 	first_item = False
 	# Indents
@@ -251,27 +251,23 @@ def to_html(tex):
 		if '\\subsubsection' in line:
 			h2 = line[len('\\subsubsection{'):-len('}')]
 			month, day = h2.split()
-			day_html += indent(0)+'<p class="mobilenav"><a href="../" class="link">' \
+			day_html += '<p class="mobilenav"><a href="../" class="link">' \
 				'(back up to '+month+')</a></p>\n'
-			day_html += indent(0)+'<div class="entry">\n'
-			day_html += indent(1)+'<h2>'+h2+'</h2>\n'
+			day_html += '<div class="entry">\n'
+			day_html += '<h2>'+h2+'</h2>\n'
 			day = day[:-len('th')]
-			month_html += indent(1)+'<h3><a href="'+day+'/">'+h2+'</a></h3>\n'
+			month_html += '<h3><a href="'+day+'/">'+h2+'</a></h3>\n'
 			# Go ahead and start the first paragraph
 		else:
-			try:
-				to_add, i, last_tags, first_item = \
-					process_line(line, i, last_tags, first_item)
-			except:
-				print(day_html)
-				print(0/0)
+			to_add, last_tags, first_item = \
+				process_line(line, last_tags, first_item)
 			day_html += to_add
 	if last_tags[-1]:
 		day_html += '</'+last_tags[-1]+'>\n'
-	day_html += indent(0)+'</div>\n' + end_html(4)
-	month_html += indent(1)+'<p>'+blurb+'\n'
-	month_html += indent(1)+'<a href="'+day+'/" class="link">(continue reading...)</a></p>\n'
-	month_html += indent(0)+'</div>\n'
+	day_html += '</div>\n' + end_html(4)
+	month_html += '<p>'+blurb+'\n'
+	month_html += '<a href="'+day+'/" class="link">(continue reading...)</a></p>\n'
+	month_html += '</div>\n'
 	return day_html, month_html
 
 # Make TIL folder if not present
@@ -281,20 +277,20 @@ if 'TIL' not in os.listdir():
 # Start the total file
 total_html = start_html(1)
 # Add in the intro
-total_html += indent(0)+'<h2>Welcome</h2>\n'
+total_html += '<h2>Welcome</h2>\n'
 intro = process_tex(open('TeX/intro.tex').read())
 parts = [part for part in intro.split('\n') if part]
 for part in parts:
-	total_html += indent(0)+'<p>'+part+'</p>\n'
+	total_html += '<p>'+part+'</p>\n'
 # Mobile navigation
-total_html += indent(0)+'<div class="mobilenav">\n'
-total_html += indent(1)+'<h2 style="margin-top: 8pt">Archive</h2>\n'
-total_html += indent(1)+'<p>You can navigate to an entry from the archive below.</p>\n'
-total_html += archive.format(indent(1))
+total_html += '<div class="mobilenav">\n'
+total_html += '<h2 style="margin-top: 8pt">Archive</h2>\n'
+total_html += '<p>You can navigate to an entry from the archive below.</p>\n'
+total_html += archive
 # Some padding
-total_html += indent(1)+'<div style="height: 6pt;"></div>\n'
-total_html += indent(0)+'</div>\n'
-total_html += indent(0)+'<div style="height: 7pt;"></div>\n'
+total_html += '<div style="height: 6pt;"></div>\n'
+total_html += '</div>\n'
+total_html += '<div style="height: 7pt;"></div>\n'
 total_html += end_html(1)
 # Make the total file
 f = open('TIL/index.html','w')
@@ -305,9 +301,9 @@ f.close()
 for year in next(os.walk('TeX'))[1]:
 	# Start the year file
 	year_html = start_html(2)
-	year_html += indent(0)+'<p class="mobilenav"><a href="../" class="link">' \
+	year_html += '<p class="mobilenav"><a href="../" class="link">' \
 		'(back up to main page) </a></p>\n'
-	year_html += indent(0)+'<h2><a href="./">'+year+'</a></h2>\n'
+	year_html += '<h2><a href="./">'+year+'</a></h2>\n'
 	# Make year if not there
 	if year not in next(os.walk('TeX'))[1]:
 		os.mkdir('TIL/'+year)
@@ -320,11 +316,11 @@ for year in next(os.walk('TeX'))[1]:
 			os.mkdir('TIL/'+year+'/'+month)
 		# Start the month file
 		month_html = start_html(3)
-		month_html += indent(0)+'<p class="mobilenav"><a href="../../" class="link">' \
+		month_html += '<p class="mobilenav"><a href="../../" class="link">' \
 			'(back up to main page)</a></p>\n'
-		month_html += indent(0)+'<h2>' + months[int(month)-1] + ' '+year+'</h2>\n'
+		month_html += '<h2>' + months[int(month)-1] + ' '+year+'</h2>\n'
 		# Increment the year file
-		year_html += indent(0)+'<h3><a href="'+month+'/">'+ \
+		year_html += '<h3><a href="'+month+'/">'+ \
 			months[int(month)-1] +'</a></h3>\n'
 		# Break up the months into days
 		alltex = open(PATH+'TeX/'+year+'/'+month+'.tex').read()
@@ -353,7 +349,7 @@ for year in next(os.walk('TeX'))[1]:
 		f.write(prettify(month_html))
 		f.close()
 	# Some padding
-	year_html += indent(0)+'<div style="height: 6pt;"></div>\n'
+	year_html += '<div style="height: 6pt;"></div>\n'
 	year_html += end_html(2)
 	# Make the year file
 	f = open('TIL/'+year+'/index.html','w')
